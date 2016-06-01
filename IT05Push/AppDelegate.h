1909a7104e75ba1be55a9b500cebd10c1ce76bc3
15249675607 <718989421@qq.com>
2016-06-02 00:00:48 +0800

    Revert "Revert "it05push""
    This reverts commit a5b7d8f7832415a813a7a9b0941856d384a83f07.

---
+++ IT05Push/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  IT05Push
+//
+//  Created by student on 16/5/25.
+//  Copyright © 2016年 李文良. All rights reserved.
+//看bxπµƒπøåµπørmdpotk®å破网
+
+#import <UIKit/UIKit.h>
+static NSString *appKey = @"5de7a33c9f898474e1cb7982";
+static NSString *channel = @"Publish channel";
+static BOOL isProduction = FALSE;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property(nonatomic,copy)NSString *name;
+@property(nonatomic,copy)NSString *name3;
+@property(nonatomic,copy)NSString *name4;
+@property(nonatomic,copy)NSString *name5;
+
+@property(nonatomic,copy)NSString *name8;
+@end
+
